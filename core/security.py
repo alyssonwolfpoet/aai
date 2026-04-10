a059e7dea78e5b0336825379d8473a8911f4cc72
Alysson <alyssos@hotmail.com>
2026-04-10 08:24:32 -0300

    feat(security): adicionar configuração via variáveis de ambiente
    - adiciona suporte ao uso de .env com load_dotenv()
    - move SECRET_KEY, ALGORITHM e ACCESS_TOKEN_EXPIRE_MINUTES para variáveis de ambiente
    - define valores padrão caso não estejam configurados
    - adiciona requirements.txt

--- core/security.py
+++ core/security.py
@@ -1,10 +1,14 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
+from dotenv import load_dotenv
+import os
 
-SECRET_KEY = "0G7fdF4ORMat7AYdsq0sIQOEarDLlkgx"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY","0G7fdF4ORMat7AYdsq0sIQOEarDLlkgx")
+ALGORITHM = os.getenv("ALGORITHM","HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES",60)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
